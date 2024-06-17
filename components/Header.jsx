a5180bfc6aa1346e77108adcc7f7e788e6846df2
Tanay <tanayjhunjhunwala.official@gmail.com>
2024-06-18 00:08:54 +0530

    using framer motion

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,8 @@ const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
         <div className="container mx-auto flex justify-between items-center">
-            <Link href="">
+            {/* Logo */}
+            <Link href="/">
                 <h1 className="text-4xl font-semibold">
                     Tanay<span className="text-accent">.</span>
                 </h1>

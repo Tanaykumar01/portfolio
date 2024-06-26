c54c53d8ab482c0c1358ee3fb529fb9c6fe0b52d
Tanay <tanayjhunjhunwala.official@gmail.com>
2024-06-26 14:13:58 +0530

    added link to the socials

--- components/Social.jsx
+++ components/Social.jsx
@@ -3,10 +3,10 @@ import Link from "next/link"
 import {FaGithub , FaLinkedin , FaYoutube , FaCode} from "react-icons/fa";
 
 const socials = [
-    {icons: <FaGithub /> , path:""},
-    {icons: <FaLinkedin /> , path:""},
-    {icons: <FaYoutube /> , path:""},
-    {icons: <FaCode /> , path:""},
+    {icons: <FaGithub /> , path:"https://github.com/Tanaykumar01"},
+    {icons: <FaLinkedin /> , path:"https://www.linkedin.com/in/tanay-kumar-jhunjhunwala-3256b9237/"},
+    {icons: <FaYoutube /> , path:"youtube.com"},
+    {icons: <FaCode /> , path:"https://leetcode.com/u/tanaykumar01/"},
 ]
 const Social = ({containerStyles , iconStyles}) => {
   return (
